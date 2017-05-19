7d8db958a2c9da5913746cb28c75626d729a88ed
Anna Kruglik <kruglik.anna@gmail.com>
2017-05-20 01:31:34 +0200

    disconnected the ble device on activity destroy

--- app/src/main/java/com/pgt/app/MainActivity.kt
+++ app/src/main/java/com/pgt/app/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : Activity() {
         if (mGatt == null) {
             return
         }
+        mGatt!!.disconnect()
         mGatt!!.close()
         mGatt = null
         super.onDestroy()
